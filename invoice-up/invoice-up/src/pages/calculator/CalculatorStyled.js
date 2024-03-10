9fd0f0bc1630b0b34548127250fbcb46e72a99c1
lupeMorales <morales.lupe@gmail.com>
2024-03-10 22:44:26 +0100

    style Calculator component

--- invoice-up/invoice-up/src/pages/calculator/CalculatorStyled.js
+++ invoice-up/invoice-up/src/pages/calculator/CalculatorStyled.js
@@ -9,7 +9,6 @@ export const CalculatorStyled = styled.main`
 
   form {
     padding: 30px;
-
     border-radius: 20px;
     box-shadow: 5px 10px 20px ${(props) => props.theme.color.grey};
   }
@@ -17,23 +16,46 @@ export const CalculatorStyled = styled.main`
   fieldset {
     border-style: none;
   }
+
+  fieldset div {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: baseline;
+  }
+
+  fieldset div p {
+    border: 1px solid ${(props) => props.theme.color.dark_text};
+  }
   legend {
     font-size: ${(props) => props.theme.fontSizes.xsmall};
-    color: ${(props) => props.theme.color.primary};
+    color: ${(props) => props.theme.color.dark_text};
   }
   label {
-    font-size: ${(props) => props.theme.fontSizes.xsmall};
+    font-size: ${(props) => props.theme.fontSizes.small};
     color: ${(props) => props.theme.color.dark_text};
   }
   section div:nth-child(1) {
     padding: 30px;
     background-color: ${(props) => props.theme.color.primary};
     border-radius: 20px;
-    z-index: 1;
   }
   section div:nth-child(2) {
     padding: 30px;
-    background-color: ${(props) => props.theme.color.light_primary};
-    border-radius: 0 0 20px 20px;
+    background-color: ${(props) => props.theme.color.light_accent};
+    border-radius: 20px;
+  }
+
+  p {
+    font-size: ${(props) => props.theme.fontSizes.small};
+  }
+  p:nth-child(4),
+  p:nth-child(2) {
+    font-size: ${(props) => props.theme.fontSizes.small};
+    background-color: white;
+    border-radius: 5px;
+    padding: 5px;
+    text-align: end;
+    margin: 8px 0 25px;
   }
 `;
