e54d15de61d3db950482ab58679c4d81a5f7761c
lupeMorales <morales.lupe@gmail.com>
2024-05-19 12:34:44 +0200

    fix render previewer

--- invoice-up/invoice-up/src/pages/dashboard/Dashboard.jsx
+++ invoice-up/invoice-up/src/pages/dashboard/Dashboard.jsx
@@ -6,13 +6,13 @@ import { DashboardFormWrapper } from "./DashboardFormWrapper";
 import { Previewer } from "../../components/layout/previewer/Previewer";
 
 export const Dashboard = () => {
-  const [dataForm, setDataForm] = useState([]);
+  const [dataForm, setDataForm] = useState({});
 
   //Info is recived by FormMyData
   //keep info in dataForm to send info by Form props
   const handleSubmit = (ev) => {
     console.log({ ev });
-    setDataForm([...dataForm, ev]);
+    setDataForm({ ...dataForm, ...ev });
   };
   return (
     <>
