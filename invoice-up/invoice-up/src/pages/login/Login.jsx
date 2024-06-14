e16ec355a8d3b61ee86a7fdc113dcbda55928b6d
lupeMorales <morales.lupe@gmail.com>
2024-06-14 21:52:49 +0200

    update InvoiceInfoItemStyled

--- invoice-up/invoice-up/src/pages/login/Login.jsx
+++ invoice-up/invoice-up/src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { LoginStyled } from "./LoginStyled";
+import { LoginStyled } from "./LoginStyled.js";
 import image from "../../assets/community.png";
 import { Button } from "../../components/atoms/buttons/Button";
 import { Header } from "../../components/layout/header/Header";
