6d22aa0da602f2223370b8d83ace9802df8c1fe5
lupeMorales <morales.lupe@gmail.com>
2024-06-16 22:11:40 +0200

    update heder style

--- invoice-up/invoice-up/src/components/layout/header/HeaderStyled.js
+++ invoice-up/invoice-up/src/components/layout/header/HeaderStyled.js
@@ -1,51 +1,11 @@
 import styled from "styled-components";
 
-/* export const HeaderStyled = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px 40px;
-  background: ${(props) => props.theme.color.primary};
-
-  h2 {
-    font-weight: 400;
-    font-family: ${(props) => props.theme.fonts.titleFont};
-    // font-size: ${(props) => props.theme.fontSizes.xsmall};
-    font-size: 3rem;
-    color: ${(props) => props.theme.color.light_text};
-  }
-
-  li {
-    list-style: none;
-    display: inline-block;
-    padding: 0 20px;
-    font-family: ${(props) => props.theme.fonts.titleFont};
-    font-size: ${(props) => props.theme.fontSizes.xsmall};
-    color: ${(props) => props.theme.color.light_text};
-    transition: all 0.2s ease-in-out;
-    &:hover {
-      background-color: ${(props) => props.theme.color.accent};
-      padding: 8px 6px;
-      border-radius: 50px;
-    }
-  }
-
-  Button {
-    margin: 0 5px;
-  }
-`;
- */
 export const HeaderStyled = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 20px 40px;
   background: ${(props) => props.theme.color.primary};
-  /*   @media (max-width: 768px) {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  } */
 
   h2 {
     font-weight: 400;
@@ -111,6 +71,7 @@ export const Nav = styled.nav`
     margin: 0 5px;
   }
   .buttons-wrapper {
+    margin-left: 50px;
     @media (max-width: 768px) {
       display: flex;
       flex-direction: column;
