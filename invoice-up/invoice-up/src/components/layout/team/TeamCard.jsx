ec906cce69d54375b34752c197b4e6736aa548ad
lupeMorales <morales.lupe@gmail.com>
2024-06-16 20:42:40 +0200

    update styles in teamCard

--- invoice-up/invoice-up/src/components/layout/team/TeamCard.jsx
+++ invoice-up/invoice-up/src/components/layout/team/TeamCard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+/* import { useEffect, useState } from "react";
 import dataTeam from "../../../data/team.json";
 import { TeamCardStyled } from "./TeamCardStyled";
 
@@ -39,16 +39,80 @@ export const TeamCard = () => {
   return (
     <>
       {infoTeam.map((team) => {
-        /*       la línea está construyendo una URL completa para la imagen, teniendo en cuenta tanto la ruta relativa de la imagen como la ubicación del componente actual. */
-
-        /*    const imgURL = new URL(`../../${testimonial.img}`, import.meta.url);
-            console.log("imgURL:", imgURL); */
+     
         return (
           <TeamCardStyled key={team.id}>
             <img src={team.image} alt={team.name} />
             <div>
               <h3>{team.name}</h3>
               <h5>{team.job} </h5>
+            </div>
+          </TeamCardStyled>
+        );
+      })}
+    </>
+  );
+};
+ */
+import { useEffect, useState } from "react";
+import dataTeam from "../../../data/team.json";
+import { TeamCardStyled } from "./TeamCardStyled";
+import icon from "../../../assets/linkedin.svg";
+/* import { faLinkedin } from "@fortawesome/free-brands-svg-icons"; */
+
+function useAssets(asset) {
+  const assets = import.meta.glob("../../../assets/*", { eager: true });
+
+  if (assets[asset]) {
+    return assets[asset].default;
+  }
+  return null;
+}
+
+export const TeamCard = () => {
+  const [infoTeam, setInfoTeam] = useState([]);
+  useEffect(() => {
+    const get = async () => {
+      const result = [];
+
+      for (let index = 0; index < dataTeam.length; index++) {
+        const element = dataTeam[index];
+        const image = useAssets("../../../assets/" + element.img);
+
+        result.push({
+          ...element,
+          image,
+        });
+      }
+
+      setInfoTeam(result);
+    };
+
+    get();
+  }, []);
+
+  return (
+    <>
+      {infoTeam.map((team) => {
+        return (
+          <TeamCardStyled key={team.id}>
+            <img src={team.image} alt={team.name} />
+            <div>
+              <h3>{team.name}</h3>
+              <h5>{team.job}</h5>
+              {team.linkedin && (
+                <a
+                  href={team.linkedin}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={icon}
+                    alt="icono de linkedin"
+                    className="linkedin-icon"
+                  />
+                </a>
+              )}
             </div>
           </TeamCardStyled>
         );
--- invoice-up/invoice-up/src/components/layout/team/TeamCardStyled.js
+++ invoice-up/invoice-up/src/components/layout/team/TeamCardStyled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+/* import styled from "styled-components";
 
 export const TeamCardStyled = styled.article`
   position: relative;
@@ -43,5 +43,157 @@ export const TeamCardStyled = styled.article`
       max-width: 290px;
       font-size: ${(props) => props.theme.fontSizes.xlarge};
     }
+  }
+`;
+ */
+/* import styled from "styled-components";
+
+export const TeamCardStyled = styled.article`
+  position: relative;
+  width: 300px;
+  overflow: hidden;
+
+  img {
+    display: block;
+    margin: auto;
+    width: 100%;
+    aspect-ratio: 2 / 3;
+    max-width: 300px;
+    object-fit: cover;
+    transition: transform 0.3s ease-in-out;
+  }
+
+  div {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 150px;
+    background-color: ${(props) => props.theme.color.primary};
+    padding: 20px;
+    clip-path: polygon(0 25%, 100% 0%, 100% 100%, 0% 100%);
+    transition: all 0.3s ease-in-out;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+  }
+
+  &:hover img {
+    transform: scale(1.1);
+  }
+
+  &:hover div {
+    height: 100%;
+    clip-path: none;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h3,
+  h5 {
+    color: ${(props) => props.theme.color.dark_text};
+    transition: opacity 0.3s ease-in-out;
+    margin-top: 50px;
+    padding-bottom: 10px;
+    font-family: ${(props) => props.theme.fonts.titleFont};
+    font-size: ${(props) => props.theme.fontSizes.normal};
+    color: ${(props) => props.theme.color.darl_text};
+    @media only screen and (max-width: 798px) {
+      max-width: 290px;
+      font-size: ${(props) => props.theme.fontSizes.xlarge};
+    }
+  }
+
+  &:hover h3,
+  &:hover h5 {
+    opacity: 1;
+  }
+
+  .linkedin-icon {
+    margin-top: 10px;
+    color: #0077b5;
+    font-size: 24px;
+    transition: opacity 0.3s ease-in-out;
+    opacity: 0;
+  }
+
+  &:hover .linkedin-icon {
+    opacity: 1;
+  }
+`;
+ */
+import styled from "styled-components";
+import linkedinIcon from "../../../assets/linkedin.svg";
+
+export const TeamCardStyled = styled.article`
+  position: relative;
+  width: 300px;
+  overflow: hidden;
+
+  img {
+    display: block;
+    margin: auto;
+    width: 100%;
+    aspect-ratio: 2 / 3;
+    max-width: 300px;
+    object-fit: cover;
+    transition: transform 0.3s ease-in-out;
+  }
+
+  div {
+    padding: 0 20px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 150px;
+    background-color: ${(props) => props.theme.color.primary};
+    clip-path: polygon(0 25%, 100% 0%, 100% 100%, 0% 100%);
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    transition: all 0.3s ease-in-out;
+  }
+
+  &:hover img {
+    transform: scale(1.1);
+  }
+
+  &:hover div {
+    height: 100%;
+    clip-path: none;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h3,
+  h5 {
+    color: ${(props) => props.theme.color.dark_text};
+    font-family: ${(props) => props.theme.fonts.titleFont};
+    font-size: ${(props) => props.theme.fontSizes.normal};
+    padding-bottom: 10px;
+    @media only screen and (max-width: 798px) {
+      font-size: ${(props) => props.theme.fontSizes.xlarge};
+    }
+  }
+
+  h5 {
+    font-weight: 300;
+    font-size: ${(props) => props.theme.fontSizes.small};
+    @media only screen and (max-width: 798px) {
+      font-size: ${(props) => props.theme.fontSizes.xlarge};
+    }
+  }
+
+  .linkedin-icon {
+    width: 24px;
+    height: 24px;
+    margin-top: 10px;
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  &:hover .linkedin-icon {
+    opacity: 1;
   }
 `;
