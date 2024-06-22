9c603a3899e8303a48c3d0fd8b27f553e8135e46
lupeMorales <morales.lupe@gmail.com>
2024-06-22 18:13:55 +0200

    responsive header dashboard

--- invoice-up/invoice-up/src/components/layout/header/HeaderDashboardStyled.js
+++ invoice-up/invoice-up/src/components/layout/header/HeaderDashboardStyled.js
@@ -1,56 +1,5 @@
 import styled from "styled-components";
 
-/* export const HeaderDashboardStyled = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px 40px;
-  background: ${(props) => props.theme.color.primary};
-  box-shadow: -1px 10px 20px 0px rgba(0, 0, 0, 0.33);
-  -webkit-box-shadow: -1px 10px 20px 0px rgba(0, 0, 0, 0.33);
-  -moz-box-shadow: -1px 10px 20px 0px rgba(0, 0, 0, 0.33);
-
-  h2 {
-    font-weight: 400;
-    font-family: ${(props) => props.theme.fonts.titleFont};
-    font-size: ${(props) => props.theme.fontSizes.large};
-    color: ${(props) => props.theme.color.light_text};
-  }
-  p {
-    font-size: ${(props) => props.theme.fontSizes.small};
-    color: ${(props) => props.theme.color.light_text};
-  }
-
-  li {
-    list-style: none;
-    display: inline-block;
-    padding: 0 40px;
-    font-family: ${(props) => props.theme.fonts.titleFont};
-    font-size: ${(props) => props.theme.fontSizes.xsmall};
-    color: ${(props) => props.theme.color.light_text};
-    transition: all 0.2s ease-in-out;
-
-    &:hover {
-      background-color: ${(props) => props.theme.color.accent};
-      padding: 8px 16px;
-      border-radius: 50px;
-    }
-  }
-
-  p:hover {
-    color: ${(props) => props.theme.color.accent};
-  }
-  div {
-    display: flex;
-    align-items: center;
-    gap: 2rem;
-  }
-  img {
-    width: 50px;
-  }
-`;
- */
-
 export const HeaderDashboardStyled = styled.header`
   display: flex;
   justify-content: space-between;
@@ -114,6 +63,7 @@ export const Nav = styled.nav`
     list-style: none;
     display: inline-block;
     padding: 0 20px;
+    text-align: center;
     font-family: ${(props) => props.theme.fonts.titleFont};
     font-size: ${(props) => props.theme.fontSizes.xsmall};
     color: ${(props) => props.theme.color.light_text};
@@ -130,16 +80,6 @@ export const Nav = styled.nav`
     flex-direction: column;
     width: 100%;
     display: ${({ isOpen }) => (isOpen ? "block" : "none")};
-
-    /*     ul {
-      flex-direction: column;
-      width: 100%;
-    }
-
-    li {
-      padding: 10px 0;
-      text-align: center;
-    } */
   }
 `;
 
